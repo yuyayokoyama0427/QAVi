7e91377e0fe317843c94ea2b90eeb13343fb0857
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-09-14 15:37:10 +0900

    コントローラーの作成

--- src/main/java/com/example/controller/BbsController.java
+++ src/main/java/com/example/controller/BbsController.java
@@ -2,10 +2,13 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +42,17 @@ public class BbsController {
 		// 記事リストをスコープに格納する
 		model.addAttribute("articleList", articleList);
 		return "view/bbsview";
+	}
+	
+	@RequestMapping(value = "/postarticle")
+	public String postarticle(@Validated ArticleForm form, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return index(model);
+		}
+		Article article = new Article();
+		BeanUtils.copyProperties(form, article);
+		articleService.insert(article);
+		return "redirect:/bbs";
 	}
 
 }
